aa4b68e14064cf7a06b67a75e04de2d86c4207f8
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-20 06:08:48 +0900

    subaru commit

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+include "dbconnect.php";
+
+$username= $_POST['username'];
+$email= $_POST['email'];
+$password= $_POST['password'];
+$phonenum= $_POST['phoneNum'];
+$address = $_POST['address'];
+
+$pdo = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8','dbuser','P@ssw0rd');
+$sql = "INSERT INTO user(username, email, password, phoneNum) VALUES ($username, $email,
+$password, $phonenum, $address)";
+$qry = $pdo->prepare($sql);
+$qry->execute();
+
+
+?>
