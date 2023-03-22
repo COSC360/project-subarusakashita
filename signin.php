2b4f790e9fa5281a833af625714805fb039001b4
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-22 14:50:39 +0900

    Update signin.php

--- signin.php
+++ signin.php
@@ -26,6 +26,7 @@ if (mysqli_num_rows($result) > 0) {
     $error = "Invalid username or password";
 }
 mysqli_close($conn);
+?>
 
 <!DOCTYPE html>
 <html>
@@ -39,22 +40,3 @@ mysqli_close($conn);
     <?php } ?>
 </body>
 </html>
-
-// $username= $_POST['username'];
-// $password= $_POST['password'];
-
-$pdo = new PDO('mysql:host=cosc360.ok.ubc.ca;dbname=user;charset=utf8','83395822','83395822');
-// $sql = "SELECT username, password FROM user WHERE username=?)";
-$sql="SELECT username FROM user";
-$sql->bindValue('i',$username);
-//$qry = $pdo->prepare($sql);
-$qry->execute();
-$result = $stmt->get_result();
-while ($row = mysql_fetch_assoc($result)) {
-    print_r($row);
-    
-    // do stuff with $row
-}
-$user = "";
-$user=$row[0];
-?>
