d41a5f1bcbbab44d684339f0e5abe53353c31aa4
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-23 08:35:58 +0900

    Update signin.php

--- signin.php
+++ signin.php
@@ -14,7 +14,7 @@ if (!$conn) {
 }
 $username = $_POST["username"];
 $password = $_POST["password"];
-$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$sql = "SELECT * FROM users WHERE username='$username' AND passwords='$password'";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // Login successful
