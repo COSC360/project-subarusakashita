28da49b7d4bafff056bd1b5618c5722be3bb5a26
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-22 14:58:30 +0900

    Checked if username already exists in db or not

--- register.php
+++ register.php
@@ -1,5 +1,14 @@
 <?php
-include "dbconnect.php";
+
+$servername = "cosc360.ok.ubc.ca";
+$username = "83395822";
+$password = "83395822";
+$dbname = "db_83395822";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 $username = $_POST['username'];
 $email = $_POST['email'];
@@ -7,11 +16,42 @@ $password = $_POST['password'];
 $phonenum = $_POST['phoneNum'];
 $address = $_POST['address'];
 
-$pdo = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'dbuser', 'P@ssw0rd');
-$sql = "INSERT INTO Users(username, email, password, phoneNum) VALUES ($username, $email,
-$password, $phonenum, $address)";
-$qry = $pdo->prepare($sql);
-$qry->execute();
+$sql = "SELECT * FROM users WHERE username='$username'";
+    $result = mysqli_query($conn, $sql);
 
-
+    
+if (mysqli_num_rows($result) > 0) {
+        // Username already exists, display error message
+        $error = "Username already exists";
+}
+else{
+    $sql = "INSERT INTO Users(username, email, password, phoneNum) VALUES ($username, $email,$password, $phonenum, $address)";
+        if (mysqli_query($conn, $sql)) {
+            // Account created successfully, redirect to login page
+            header("Location: login.php");
+            exit;
+        }
+        else{
+            $error = "Account creation failed";
+        }
+}
+// $pdo = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'dbuser', 'P@ssw0rd');
+// $sql = "INSERT INTO Users(username, email, password, phoneNum) VALUES ($username, $email,
+// $password, $phonenum, $address)";
+// $qry = $pdo->prepare($sql);
+// $qry->execute();
+mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Account Validation</title>
+</head>
+<body>
+    <h1>Create Account Validation Check...</h1>
+    <?php if (isset($error)) { ?>
+        <p><?php echo $error; ?></p>
+    <?php } ?>
+</body>
+</html>
