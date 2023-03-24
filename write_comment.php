aebecd72d0f31833a0a902c53ca3d15d15c965fa
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-25 08:32:15 +0900

    Update write_comment.php

--- write_comment.php
+++ write_comment.php
@@ -1,13 +1,13 @@
 <?php
         if (isset($_SESSION['username'])) {
 
-            echo "<form method = processComment.php action='post'>
+            echo "<form action = 'processComment.php' method='post'>
                 <fieldset>
                     <label for='comment'>Comment</label>
                 <textarea id='comment' name='comment' rows='15' placeholder='Write acomment here'
                     required></textarea>
                 <br>
-                <input type='hidden' name = 'articleId' value='<?php echo $articleId ?>'>
+                <input type='hidden' name = 'articleId' value= echo $articleId>
                 <input type='submit' value='Post Comment'>
                 </fieldset> 
             ";
