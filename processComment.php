b00244448894a2592026cac3674c8372db8c1342
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-25 08:49:20 +0900

    Update processComment.php

--- processComment.php
+++ processComment.php
@@ -12,19 +12,20 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    // echo($_POST['articleId']);
-    // echo($_POST['comment']);
     $username = $_SESSION('username');
     $articleId = $_POST['articleId'];
     $comment = $_POST['comment'];
-    $sql = "INSERT INTO Comments(username, articleId, commentBody) VALUES('$username','$articleId','$comment')";
-    if (mysqli_query($conn, $sql)) {
-       // echo("<script>alert('Comment Uploaded. Going back to main page') </script>");
-        header("Location: main.php");
-        exit;
-    }
-    else{
-        $error = "Comment Upload failed";
-    }
+    echo($comment);
+    echo($articleId);
+    echo($username);
+    // $sql = "INSERT INTO Comments(username, articleId, commentBody) VALUES('$username','$articleId','$comment')";
+    // if (mysqli_query($conn, $sql)) {
+    //     //echo("<script>alert('Comment Uploaded. Going back to main page') </script>");
+    //     header("Location: main.php");
+    //     exit;
+    // }
+    // else{
+    //     $error = "Comment Upload failed";
+    // }
     mysqli_close($conn);
 ?>
