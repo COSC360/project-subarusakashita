4a9d6a6038c431c08a82b65142a71fd82c501f05
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-25 08:33:48 +0900

    Update processComment.php

--- processComment.php
+++ processComment.php
@@ -11,6 +11,6 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo(_POST['articleId']);
-    echo(_POST['comment']);
+    echo($_POST['articleId']);
+    echo($_POST['comment']);
 ?>
