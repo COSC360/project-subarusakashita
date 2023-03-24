d559674de2834b9838c8500c72e802af295beb85
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-03-24 14:40:35 -0700

    Testing ads

--- include/ad_long.php
+++ include/ad_long.php
@@ -1,5 +1,5 @@
 <?php
-echo ('<a href="#"><img src="../ads/long/' . rand(1, 4) . '.png" alt="Advertisement"></a>');
+// echo ('<a href="#"><img src="../ads/long/' . rand(1, 4) . '.png" alt="Advertisement"></a>');
 echo ('<a href="#"><img src="../ads/long/1.png" alt="Advertisement"></a>');
 echo ('<a href="#"><img src="ads/long/1.png" alt="Advertisement"></a>');
 ?>
