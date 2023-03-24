1fabbf99e1ba9273219e628d9439db083d5d087e
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-03-24 14:42:47 -0700

    Testing ads

--- include/ad_long.php
+++ include/ad_long.php
@@ -1,4 +1,4 @@
 <?php
 // echo ('<a href="#"><img src="../ads/long/' . rand(1, 4) . '.png" alt="Advertisement"></a>');
-echo ('<a href="#"><img src="ads/long/1.png" alt="Advertisement"></a>');
+echo ('<a href="#"><img src="ads/long/' . rand(1,4) . '.png" alt="Advertisement"></a>');
 ?>
