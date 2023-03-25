bbb5c2823e0425db66c44af08f584332d4376695
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-26 07:31:05 +0900

    Update article.php

--- article.php
+++ article.php
@@ -78,16 +78,16 @@ if (isset($_GET['articleTitle'])) {
 
             include "include/ad_long.php";
 
-            // $sql = "SELECT username, commentBody FROM Comments WHERE articleId = '$articleId'";
-            // $result = mysqli_query($conn, $sql);
-            //  if (mysqli_num_rows($result) > 0) {
-            // while($row = mysqli_fetch_assoc($result)) {
-            //      echo "<p>". $row["username"] .": ". $row["commentBody"] "</p>";
+            $sql = "SELECT username, commentBody FROM Comments WHERE articleId = '$articleId'";
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                 echo "<p>". $row["username"] . $row["commentBody"]. "</p>";
            
-            //  }
-            // } else {
-            //     echo "No comments. Comment!!";
-            // }
+             }
+            } else {
+                echo "No comments. Comment!!";
+            }
 
             //     echo ("<h3>" . $row['articleBody'] . "</h3>");
 
