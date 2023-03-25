db28e770d23edc25078ed3bf5c474fbbc9d278bc
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-26 05:28:18 +0900

    Create showComment.php

---
+++ showComment.php
@@ -0,0 +1,17 @@
+<?php
+        $servername = "cosc360.ok.ubc.ca";
+        $server_username = "83395822";
+        $server_password = "83395822";
+        $dbname = "db_83395822";
+    
+    // Create connection
+    $conn = new mysqli($servername, $server_username, $server_password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    
+    
+?>
