f4847fe2a0b2aed2261b944c189214fbdb7f9724
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-03-25 16:51:07 -0700

    Fixing format

--- include/top_left.php
+++ include/top_left.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="css/default.css">
-<link rel="stylesheet" href="css/login.css">
 
 <?php
 session_start();
@@ -8,11 +6,11 @@ echo ('
 <div id=top>
 ');
 
-if (isset($_SESSION['username'])) {
-    echo ('<a href="profile.php?username=' . $_SESSION['username'] . '>' . $_SESSION['username'] . '</a>');
-} else {
-    echo ('<a href="login.php">Log in</a>');
-}
+// if (isset($_SESSION['username'])) {
+//     echo ('<a href="profile.php?username=' . $_SESSION['username'] . '>' . $_SESSION['username'] . '</a>');
+// } else {
+//     echo ('<a href="login.php">Log in</a>');
+// }
 
 echo ('
 <form action="search.php" method="get">
