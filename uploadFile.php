653ab5815aad5a9c4a15b85b35a0fb237070fcc3
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-26 08:51:15 +0900

    Update uploadFile.php

--- uploadFile.php
+++ uploadFile.php
@@ -1,4 +1,4 @@
-<form action="processFile.php" action="post">
+<form enctype = "multipart-form-data" action="processFile.php" action="post">
 
 <input type="hidden" name="MAX_FILE_SIZE" value="5" />
 <input name="img" type="file" accept="image/*" />
