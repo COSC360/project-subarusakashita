f243f92fbc6a55f08f43dabdce09bd173fbf5d33
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-26 09:41:05 +0900

    Update uploadFile.php

--- uploadFile.php
+++ uploadFile.php
@@ -1,69 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Image Gallery</title>
-</head>
-<body>
-  <h1>Image Gallery</h1>
+<?php
+// Database configuration
+$servername = "cosc360.ok.ubc.ca";
+$username = "83395822";
+$password = "83395822";
+$dbname = "db_83395822";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle image upload
+if (isset($_POST['submit'])) {
+  $image = $_FILES['image'];
   
-  <?php
-  // Connect to database
-  $servername = "cosc360.ok.ubc.ca";
-  $username = "83395822";
-  $password = "83395822";
-  $dbname = "db_83395822";
-  
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  // Check if file is an image
+  $fileType = strtolower(pathinfo($image['name'], PATHINFO_EXTENSION));
+  if (!in_array($fileType, array('jpg', 'jpeg', 'png', 'gif'))) {
+    die("File is not an image.");
   }
   
-  // Insert form data into database
-  if (isset($_POST['submit'])) {
-    $filename = $_FILES['image']['name'];
-    $filepath = $_FILES['image']['tmp_name'];
-    $destination = "uploads/" . $filename;
-    
-    if (!empty($filename)) {
-      if (move_uploaded_file($filepath, $destination)) {
-        $sql = "INSERT INTO images (file_name, file_path) VALUES ('$filename', '$destination')";
-        if ($conn->query($sql) === TRUE) {
-          echo "<p>Image uploaded successfully</p>";
-        } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-      } else {
-        echo "<p>There was an error uploading the file</p>";
-      }
-    } else {
-      echo "<p>Please select an image to upload</p>";
-    }
+  // Generate unique file name
+  $fileName = uniqid() . '.' . $fileType;
+  
+  // Move file to server
+  $uploadDir = "uploads/";
+  $uploadPath = $uploadDir . $fileName;
+  move_uploaded_file($image['tmp_name'], $uploadPath);
+  
+  // Insert file name and path into database
+  $sql = "INSERT INTO images (file_name, file_path) VALUES ('$fileName', '$uploadPath')";
+  $conn->query($sql);
+}
+
+// Display uploaded images from database
+$sql = "SELECT file_name, file_path FROM images";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    $fileName = $row['file_name'];
+    $filePath = $row['file_path'];
+    echo "<img src=\"$filePath\" alt=\"$fileName\" />";
   }
-  
-  // Display images from database
-  $sql = "SELECT id, file_name, file_path FROM images";
-  $result = $conn->query($sql);
-  
-  if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-      $id = $row['id'];
-      $filename = $row['file_name'];
-      $filepath = $row['file_path'];
-      echo "<div>";
-      echo "<img src=\"$filepath\" alt=\"$filename\">";
-      echo "<p>$filename</p>";
-      echo "</div>";
-    }
-  } else {
-    echo "<p>No images found</p>";
-  }
-  
-  $conn->close();
-  ?>
-  
-  <form method="post" enctype="multipart/form-data">
-    <input type="file" name="image">
-    <input type="submit" name="submit" value="Upload">
-  </form>
-</body>
-</html>
+} else {
+  echo "No images found";
+}
+
+// Close database connection
+$conn->close();
+?>
