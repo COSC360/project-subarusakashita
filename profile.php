42abc5b70e4eb4335f29ea8cf5485bdede40ebcc
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-03-25 17:56:08 -0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -48,7 +48,7 @@ if ($conn->connect_error) {
         
         $session_username = $_SESSION['username'];
         $sql = "SELECT * FROM users WHERE username = '$session_username'";
-        echo $sql;
+        // echo $sql;
         $sql2 = "SELECT * FROM users";
 
         $result = mysqli_query($conn, $sql);
@@ -56,6 +56,25 @@ if ($conn->connect_error) {
             while ($row = mysqli_fetch_assoc($result)) {
                 echo ("<h3>Username: " . $row['username'] . "</h3><br>");
                 //username is unchangeable
+        
+                echo ("<form action=\"profile.php?username=" . $_SESSION['username'] . "\" method=\"post\">");
+                //form open, submitted change will change the variables at very top of page
+        
+                echo ("<label for=\"emailNew\">Email</label><br>");
+                echo ("<input type=\"email\" id=\"emailNew\" name=\"emailNew\" placeholder=\"" . $row['email'] . "\"><br><br>");
+                //current email in placeholder, email will update when typed into the input
+        
+                echo ("<label for=\"phoneNumNew\">Phone number</label><br>");
+                echo ("<input type=\"tel\" id=\"phoneNumNew\" name=\"phoneNumNew\" placeholder=\"" . $row['phoneNum'] . "\"><br><br>");
+
+                echo ("<label for=\"addressNew\">Address</label><br>");
+                echo ("<input type=\"text\" id=\"addressNew\" name=\"addressNew\" placeholder=\"" . $row['address'] . "\"><br><br>");
+
+                echo ("<label for=\"postalCodeNew\">Postal Code</label><br>");
+                echo ("<input type=\"text\" id=\"postalCodeNew\" name=\"postalCodeNew\" placeholder=\"" . $row['postalCode'] . "\"><br><br>");
+
+                echo ("</form>");
+                //close form
             }
         } else {
             echo "No rows";
@@ -63,25 +82,8 @@ if ($conn->connect_error) {
 
 
 
-        // echo ("<form action=\"profile.php?username=" . $_SESSION['username'] . "\" method=\"post\">");
-        // //form open, submitted change will change the variables at very top of page
-        
-        // echo ("<label for=\"emailNew\">Email</label><br>");
-        // echo ("<input type=\"email\" id=\"emailNew\" name=\"emailNew\" placeholder=\"" . $row['email'] . "\"><br><br>");
-        // //current email in placeholder, email will update when typed into the input
-        
-        // echo ("<label for=\"phoneNumNew\">Phone number</label><br>");
-        // echo ("<input type=\"tel\" id=\"phoneNumNew\" name=\"phoneNumNew\" placeholder=\"" . $row['phoneNum'] . "\"><br><br>");
-        
-        // echo ("<label for=\"addressNew\">Address</label><br>");
-        // echo ("<input type=\"text\" id=\"addressNew\" name=\"addressNew\" placeholder=\"" . $row['address'] . "\"><br><br>");
-        
-        // echo ("<label for=\"postalCodeNew\">Postal Code</label><br>");
-        // echo ("<input type=\"text\" id=\"postalCodeNew\" name=\"postalCodeNew\" placeholder=\"" . $row['postalCode'] . "\"><br><br>");
-        
-        // echo ("</form>");
-        // //close form
-        
+
+
         // if ($row['isAdmin'] == true) {
         //     echo ("<a href=\"adminControl.php\">Admin Control Page</a>");
         // }
