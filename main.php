41bf69271745de324e6f400a43d0158645a879ab
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-03-25 17:06:41 -0700

    Update main.php

--- main.php
+++ main.php
@@ -37,11 +37,11 @@ if (!$conn) {
         <div id="tag">
             <?php
             //$sql1 = "SELECT tagId, tagName FROM Tags ORDER BY articleNumber LIMIT 10";
-           // $result1 = mysqli_query($conn, $sql1);
-
+            // $result1 = mysqli_query($conn, $sql1);
+            
             //while ($row = sqlsrv_fetch_array($result1, SQLSRV_FETCH_ASSOC)) {
-              //  echo ("<a href='tag.php?tagId=" . $row['tagId'] . "&tagName=" . $row['tagName'] . "'>" . $row['tagName'] . "</a>");
-           // }
+            //  echo ("<a href='tag.php?tagId=" . $row['tagId'] . "&tagName=" . $row['tagName'] . "'>" . $row['tagName'] . "</a>");
+            // }
             ?>
         </div>
         <?php include "include/ad_long.php"; ?>
@@ -51,7 +51,7 @@ if (!$conn) {
             <ul>
                 <?php
                 $sql2 = "SELECT articleId, articleTitle FROM Articles ORDER BY views LIMIT 6";
-                $result2 = mysqli_query($conn, $sql2 );
+                $result2 = mysqli_query($conn, $sql2);
                 while ($row = sqlsrv_fetch_array($result2, SQLSRV_FETCH_ASSOC)) {
                     echo ("<li><a href='article.php?articleId=" . $row['articleId'] . "&articleTitle=" . $row['articleTitle'] . "'>" . $row['articleTitle'] . "</a></li>");
                 }
@@ -59,8 +59,10 @@ if (!$conn) {
             </ul>
         </div>
     </div>
-    <?php include "include/footer.php" ?>
-
+    <!-- <?php include "include/footer.php" ?> -->
+    <footer>
+        <p>Footer Copyright info and etc</p>
+    </footer>
 </body>
 
 </html>
