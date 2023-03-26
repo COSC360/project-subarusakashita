3151720dc0cef6143c8a0119ba0f700e3b0bf2c8
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-03-26 12:01:42 -0700

    Update article.php

--- article.php
+++ article.php
@@ -69,8 +69,8 @@ $user = $_SESSION['username'];
             // Comments
             $sql4 = "SELECT * FROM Comments WHERE articleId = '$articleId'";
             $result4 = mysqli_query($conn, $sql4);
-            echo ("<br><h2>Comments</h2><a href='write_comment.php'>[Post new comment]</a>");
-            echo ("<h3><a href=''" . "</h3>");
+            echo ("<br><h2>Comments</h2>");
+            echo ("<h3><a href='write_comment.php?articleId=" . $articleId . "'>[Post new comment]</a></h3>");
             if (mysqli_num_rows($result4) > 0) {
                 while ($row = mysqli_fetch_assoc($result4)) {
                     echo ('<h3>' . $row["username"] . ' - ' . $row["commentBody"] . '</h3>');
