42aa5bcd36e1cd61fe07a1349ba07e016e038408
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-27 07:51:40 +0900

    Update register.js

--- js/register.js
+++ js/register.js
@@ -7,24 +7,24 @@ document.getElementById("registerform").addEventListener("submit", function(even
   
     // Validate the form data
     if (formData.get("username") == "") {
-      document.getElementById("error-message").textContent = "Please enter your username.";
+      document.getElementById("errormessage").textContent = "Please enter your username.";
       return;
     }
     if (formData.get("email") == "") {
-        document.getElementById("error-message").textContent = "Please enter your email.";
+        document.getElementById("errormessage").textContent = "Please enter your email.";
         return;
       }
     if (formData.get("password") == "") {
-        document.getElementById("error-message").textContent = "Please enter your password.";
+        document.getElementById("errormessage").textContent = "Please enter your password.";
         return;
     }
     if (formData.get("password_conf") == "") {
-        document.getElementById("error-message").textContent = "Please enter your password confirmation.";
+        document.getElementById("errormessage").textContent = "Please enter your password confirmation.";
         return;
       }
   
     // Clear the error message
-    document.getElementById("error-message").textContent = "";
+    document.getElementById("errormessage").textContent = "";
   
     // Submit the form data if it is valid
     this.submit();
