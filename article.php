11b0b153abc88eec32506d2684a370bca162733d
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-27 06:44:53 +0900

    Update article.php

--- article.php
+++ article.php
@@ -82,8 +82,8 @@ $user = $_SESSION['username'];
             if (isset($_SESSION['username'])) {
                 // if not logged in
                 // pop-up to encourage log in
-                echo '<form action ="writecomment.php" method = "post">
-                      <input type="text" id="commentBody" name="commentBody" placeholder="Write comment here" required>
+                echo '<form action ="processComment.php" method = "post">
+                      <input type="text" id="comment" name="comment" placeholder="Write comment here" required>
                       <input type = "hidden" name = "username" value = echo $user>
                       <input type = "hidden" name = "articleId" value = echo $articleId>
                       <br>
