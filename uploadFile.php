2d824c398cc83de4d8af250d99f1a23ff43916bc
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-26 09:17:43 +0900

    Edited file

--- uploadFile.php
+++ uploadFile.php
@@ -1,18 +1,54 @@
-<script>
-function validateForm() {
-  var imageField = document.getElementById("image");
-  var file = imageField.files[0];
-  if (!file || !file.type.match('image.*')) {
-    alert("Please select an image file.");
-    return false;
+<?php
+// Check if form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get form data
+  $formName = $_POST['formName'];
+  $formPath = $_FILES['formFile']['tmp_name'];
+  $formType = $_FILES['formFile']['type'];
+  
+  // Check if file is an image
+  $allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+  if (!in_array($formType, $allowedTypes)) {
+    die('Error: File must be an image (JPEG, PNG, or GIF)');
   }
-  return true;
+  
+  // Insert form name and path into database
+  $servername = "cosc360.ok.ubc.ca";
+  $username = "83395822";
+  $password = "83395822";
+  $dbname = "db_83395822";
+  
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $sql = "INSERT INTO images (file_name, file_path) VALUES ('$formName', '$formPath')";
+  if ($conn->query($sql) === TRUE) {
+    echo "Form submitted successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
 }
-</script>
+?>
 
-<form action="processFile.php" method="post" enctype="multipart/form-data" onsubmit="return validateForm()">
-  <label for="image">Select an image:</label>
-  <input type="file" id="image" name="image" accept="image/*">
-  <br>
-  <input type="submit" value="Submit">
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Upload Form</title>
+</head>
+<body>
+  <h1>Upload Form</h1>
+  <form method="post" enctype="multipart/form-data">
+    <label for="formName">Form Name:</label>
+    <input type="text" name="formName" required>
+    <br>
+    <label for="formFile">Select an image file:</label>
+    <input type="file" name="formFile" accept="image/*" required>
+    <br>
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
