9dce15a641c56971589d7738e4d158869f73dbb4
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-03-26 14:07:25 +0900

    Update showComment.php

--- showComment.php
+++ showComment.php
@@ -1,8 +1,8 @@
 <?php
-    $sql = "SELECT username, commentBody FROM Comments WHERE articleId = '$articleId'";
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        while($row = mysqli_fetch_assoc($result)) {
+    $sqlcomment = "SELECT username, commentBody FROM Comments WHERE articleId = '$articleId'";
+    $resultcomment = mysqli_query($conn, $sqlcomment);
+    if (mysqli_num_rows($resultcomment) > 0) {
+        while($row = mysqli_fetch_assoc($resultcomment)) {
             echo "<p>". $row["username"] .": ". $row["commentBody"] ."</p>";
            
         }
