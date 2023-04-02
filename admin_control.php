6486f90a3f090b872d11cb9537e0072dcdb39a32
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-02 14:06:33 -0700

    Update admin_control.php

--- admin_control.php
+++ admin_control.php
@@ -8,6 +8,8 @@ if (isset($_SESSION['username'])) {
 echo '<script>alert("username is ' . $username . '");</script>';
 
 $sql = "SELECT * FROM users WHERE username = '$username'";
+echo '<script>alert("sql is ' . $sql . '");</script>';
+
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
