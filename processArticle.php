2c8bfb0ec90573c9f131829febfc4060e4b85a0c
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-01 22:15:34 -0700

    Update processArticle.php

--- processArticle.php
+++ processArticle.php
@@ -10,40 +10,37 @@ $conn = new mysqli($servername, $server_username, $server_password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-else{
-echo "Connected successfully";
-}
+    die("Connection failed: " . $conn->connect_error);
+} else {
 
-$title = $_POST["newArticleTitle"];
-if($_POST["category"]==="Academic"){
-    $category = 1;
-}
-else if($_POST["category"]==="Lifestyle"){
-    $category = 2;
-}
 
-$tag=$_POST["tag"];
-if($_POST["tag"] === "Professor"){
-    $tag = 1;
-}else if($_POST["tag"]==="Course"){
-    $tag = 2;
-}else if($_POST["tag"]==="Study"){
-    $tag = 3;
-}else if($_POST["tag"]==="Laundry"){
-    $tag = 4;
-}else if($_POST["tag"]==="Emergencies"){
-    $tag = 5;
-}else if($_POST["tag"]==="Cooking"){
-    $tag = 6;
-}else if($_POST["tag"]==="Grocery"){
-    $tag = 7;
-}
+    $title = $_POST["newArticleTitle"];
+    if ($_POST["category"] === "Academic") {
+        $category = 1;
+    } else if ($_POST["category"] === "Lifestyle") {
+        $category = 2;
+    }
 
-$body = $_POST["newArticleBody"];
-$username = $_SESSION['username'];
-// $sql = "INSERT INTO Articles(articleTitle, username, categoryId, tagId, articleBody) VALUES ('$title','$username','$category','$tag','$body')";
+    $tag = $_POST["tag"];
+    if ($_POST["tag"] === "Professor") {
+        $tag = 1;
+    } else if ($_POST["tag"] === "Course") {
+        $tag = 2;
+    } else if ($_POST["tag"] === "Study") {
+        $tag = 3;
+    } else if ($_POST["tag"] === "Laundry") {
+        $tag = 4;
+    } else if ($_POST["tag"] === "Emergencies") {
+        $tag = 5;
+    } else if ($_POST["tag"] === "Cooking") {
+        $tag = 6;
+    } else if ($_POST["tag"] === "Grocery") {
+        $tag = 7;
+    }
+
+    $body = $_POST["newArticleBody"];
+    $username = $_SESSION['username'];
+    // $sql = "INSERT INTO Articles(articleTitle, username, categoryId, tagId, articleBody) VALUES ('$title','$username','$category','$tag','$body')";
 // if (mysqli_query($conn, $sql)) {
 //     header("Location: main.php");
 //     exit;
@@ -53,23 +50,23 @@ $username = $_SESSION['username'];
 // }
 
 
-try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
-    $sql = "INSERT INTO Articles(articleTitle, username, categoryId, tagId, articleBody) VALUES (?,?,?,?,?)";
-    $statement = $pdo->prepare($sql);
-    $statement->bindValue(1, $title);
-    $statement->bindValue(2, $username);
-    $statement->bindValue(3, $category);
-    $statement->bindValue(4, $tag);
-    $statement->bindValue(5, $body);
-    $statement->execute();
-    echo '<script>alert("Article Published");</script>';
-} catch (Exception $e) {
-    echo '<script>alert("Error publishing article");</script>';
+    try {
+        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
+        $sql = "INSERT INTO Articles(articleTitle, username, categoryId, tagId, articleBody) VALUES (?,?,?,?,?)";
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(1, $title);
+        $statement->bindValue(2, $username);
+        $statement->bindValue(3, $category);
+        $statement->bindValue(4, $tag);
+        $statement->bindValue(5, $body);
+        $statement->execute();
+        echo '<script>alert("Article Published");</script>';
+    } catch (Exception $e) {
+        echo '<script>alert("Error publishing article");</script>';
+    }
+
+    mysqli_close($conn);
 }
-
-mysqli_close($conn);
-
-echo "Back to <a href='article.php?articleId=" . $articleId . "'>Article</a>";
+echo "Back to <a href='profile.php?username=" . $username . "'>Profile Page</a>";
 
 ?>
