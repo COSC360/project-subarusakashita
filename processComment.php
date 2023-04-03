259d0c79b8b3ee30c39f00822e3774cedaf4d05f
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-03 14:30:51 -0700

    Update processComment.php

--- processComment.php
+++ processComment.php
@@ -20,22 +20,36 @@ $sql1 = "SELECT * FROM users WHERE username='$username'";
 $result1 = mysqli_query($conn, $sql1);
 if (mysqli_num_rows($result1) > 0) {
     while ($row = mysqli_fetch_assoc($result1)) {
-        echo ("User is " . $row['isDisabled']);
+        if ($row['isDisabled === 1']) {
+            // user account is disabled
+            echo ("Disabled users cannot post comments");
+
+        } else {
+            // user account is not disabled
+            try {
+                $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
+
+                $sql2 = "INSERT INTO Comments(username, articleId, commentBody) VALUES(?,?,?)";
+                $statement = $pdo->prepare($sql2);
+                $statement->bindValue(1, $username);
+                $statement->bindValue(2, $articleId);
+                $statement->bindValue(3, $comment);
+                $statement->execute();
+
+                $sql3 = "UPDATE Articles SET commentNum = commentNum+1 WHERE articleId=?";
+                $statement = $pdo->prepare($sql3);
+                $statement->bindValue(1, $articleId);
+                $statement->execute();
+
+                echo '<script>alert("Comment Posted");</script>';
+            } catch (Exception $e) {
+                echo '<script>alert("Error posting comment");</script>';
+            }
+        }
     }
 }
 
-try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
-    $sql = "INSERT INTO Comments(username, articleId, commentBody) VALUES(?,?,?)";
-    $statement = $pdo->prepare($sql);
-    $statement->bindValue(1, $username);
-    $statement->bindValue(2, $articleId);
-    $statement->bindValue(3, $comment);
-    $statement->execute();
-    echo '<script>alert("Comment Posted");</script>';
-} catch (Exception $e) {
-    echo '<script>alert("Error posting comment");</script>';
-}
+
 mysqli_close($conn);
 
 echo "Back to <a href='article.php?articleId=" . $articleId . "'>Article</a>";
