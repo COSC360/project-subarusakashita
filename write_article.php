4e91e21a8e5cd54e5dd3fdf3a1aee8db9ee77283
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-03 22:44:22 -0700

    Update write_article.php

--- write_article.php
+++ write_article.php
@@ -20,7 +20,7 @@ $sql1 = "SELECT * FROM users WHERE username='$username'";
 $result1 = mysqli_query($conn, $sql1);
 if (mysqli_num_rows($result1) > 0) {
     while ($row = mysqli_fetch_assoc($result1)) {
-        if ($row['isDisabled' === '1']) {
+        if ($row['isDisabled'] === '1') {
             // if user account is disabled
             echo '<script>alert("Account is disabled");</script>';
             header("Location: main.php");
