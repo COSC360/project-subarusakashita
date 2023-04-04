00b09b370e5a8010e75f870f74b845bbc0eaf60f
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-03 17:23:27 -0700

    Update article.php

--- article.php
+++ article.php
@@ -93,7 +93,7 @@ $user = $_SESSION['username'];
                     $disabled = $row['isDisabled'];
                 }
             }
-            if (isset($user) && $disabled !== '0') {
+            if (isset($user) && $disabled !== false) {
                 $articleId = $articleId = $_GET['articleId'];
                 // if logged in
                 echo '<form action ="processComment.php" method = "post">
