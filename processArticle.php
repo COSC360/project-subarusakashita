ba9e0d96b38970db3e2b14061432947570c0e394
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-03 22:44:44 -0700

    Update processArticle.php

--- processArticle.php
+++ processArticle.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
         header("Location: main.php");
         exit;
     }
-    
+
     $username = null;
     if (isset($_SESSION['username'])) {
         $username = $_SESSION['username'];
@@ -31,7 +31,7 @@ if ($conn->connect_error) {
     $result1 = mysqli_query($conn, $sql1);
     if (mysqli_num_rows($result1) > 0) {
         while ($row = mysqli_fetch_assoc($result1)) {
-            if ($row['isDisabled' === '1']) {
+            if ($row['isDisabled'] === '1') {
                 // if user account is disabled
                 echo '<script>alert("Account is disabled");</script>';
                 header("Location: main.php");
