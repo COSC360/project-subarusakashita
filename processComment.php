cff9414ad58b25dc541386dae91f52be7f2525bb
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-03 22:14:19 -0700

    Update processComment.php

--- processComment.php
+++ processComment.php
@@ -16,39 +16,40 @@ $username = $_SESSION['username'];
 $articleId = $_POST['articleId'];
 $comment = $_POST['comment'];
 
+$username = $_SESSION['username'];
+
 $sql1 = "SELECT * FROM users WHERE username='$username'";
 $result1 = mysqli_query($conn, $sql1);
 if (mysqli_num_rows($result1) > 0) {
     while ($row = mysqli_fetch_assoc($result1)) {
-        if ($row['isDisabled'] === '1') {
-            // user account is disabled
-            echo '<script>alert("Disabled users cannot post comments");</script>';
-
-        } else {
-            // user account is not disabled
-            try {
-                $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
-
-                $sql2 = "INSERT INTO Comments(username, articleId, commentBody) VALUES(?,?,?)";
-                $statement = $pdo->prepare($sql2);
-                $statement->bindValue(1, $username);
-                $statement->bindValue(2, $articleId);
-                $statement->bindValue(3, $comment);
-                $statement->execute();
-
-                $sql3 = "UPDATE Articles SET commentNum = commentNum+1 WHERE articleId=?";
-                $statement = $pdo->prepare($sql3);
-                $statement->bindValue(1, $articleId);
-                $statement->execute();
-
-                echo '<script>alert("Comment Posted");</script>';
-            } catch (Exception $e) {
-                echo '<script>alert("Error posting comment");</script>';
-            }
+        if ($row['isDisabled === 1']) {
+            // if user account is disabled
+            echo '<script>alert("Account is disabled");</script>';
+            header("Location: main.php");
+            exit;
         }
     }
 }
+// user account is not disabled
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
 
+    $sql2 = "INSERT INTO Comments(username, articleId, commentBody) VALUES(?,?,?)";
+    $statement = $pdo->prepare($sql2);
+    $statement->bindValue(1, $username);
+    $statement->bindValue(2, $articleId);
+    $statement->bindValue(3, $comment);
+    $statement->execute();
+
+    $sql3 = "UPDATE Articles SET commentNum = commentNum+1 WHERE articleId=?";
+    $statement = $pdo->prepare($sql3);
+    $statement->bindValue(1, $articleId);
+    $statement->execute();
+
+    echo '<script>alert("Comment Posted");</script>';
+} catch (Exception $e) {
+    echo '<script>alert("Error posting comment");</script>';
+}
 
 mysqli_close($conn);
 
