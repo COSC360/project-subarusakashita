99d0ce97889f93f6c796367b332a1b490438697b
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-03 17:09:35 -0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -72,109 +72,109 @@ if (!isset($_SESSION['username'])) {
             }
         }
 
-        if ($isDisabled) {
-            echo "This account is disabled. <br>";
-            echo ("<a href='logout.php'>Log Out</a>");
+
+        if (
+            !empty($_POST['emailNew']) || !empty($_POST['phoneNumNew']) || !empty($_POST['addressNew'])
+            || !empty($_POST['postalCodeNew']) || !empty($_POST['passwordNew'])
+        ) {
+
+            // update info from form
+            if (!empty($_POST['emailNew'])) {
+                $email = $_POST['emailNew'];
+            }
+            if (!empty($_POST['phoneNumNew'])) {
+                $phoneNum = $_POST['phoneNumNew'];
+            }
+            if (!empty($_POST['addressNew'])) {
+                $address = $_POST['addressNew'];
+            }
+            if (!empty($_POST['postalCodeNew'])) {
+                $postalCode = $_POST['postalCodeNew'];
+            }
+            if (!empty($_POST['passwordNew'])) {
+                $password = $_POST['passwordNew'];
+            }
+
+            try {
+                $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
+                $sql2 = "UPDATE users SET email=?, phoneNum=?, address=?, postalCode=?, passwords=? WHERE username=?";
+                $statement = $pdo->prepare($sql2);
+                $statement->bindValue(1, $email);
+                $statement->bindValue(2, $phoneNum);
+                $statement->bindValue(3, $address);
+                $statement->bindValue(4, $postalCode);
+                $statement->bindValue(5, md5($password));
+                $statement->bindValue(6, $session_username);
+                $statement->execute();
+                echo '<script>alert("Personal information updated successfully");</script>';
+            } catch (Exception $e) {
+                echo '<script>alert("Error updating personal info");</script>';
+            }
+        }
+
+        // Show updated info in placeholder
+        // Form to let users change info
+        $sql3 = "SELECT * FROM users WHERE username = '$session_username'";
+        $result3 = mysqli_query($conn, $sql3);
+        if (mysqli_num_rows($result3) > 0) {
+            while ($row = mysqli_fetch_assoc($result3)) {
+                echo ("<h3>Username: " . $row['username'] . "</h3>");
+                //username is unchangeable
+        
+                echo ("<form action='profile.php?username=" . $_SESSION['username'] . "' method='post'>");
+                //form open, submitted change will change the variables above
+        
+                echo ("<label for='emailNew'>Email</label><br>");
+                echo ("<input type='email' id='emailNew' name='emailNew' placeholder='" . $row['email'] . "'><br><br>");
+                //current email in placeholder, email will update when typed into the input
+        
+                echo ("<label for='passwordNew'>Password</label><br>");
+                echo ("<input type='password' id='passwordNew' name='passwordNew' placeholder='●●●●●●●'><br><br>");
+
+                echo ("<label for='phoneNumNew'>Phone number</label><br>");
+                echo ("<input type='tel' id='phoneNumNew' name='phoneNumNew' placeholder='" . $row['phoneNum'] . "'><br><br>");
+
+                echo ("<label for='addressNew'>Address</label><br>");
+                echo ("<input type='text' id='addressNew' name='addressNew' placeholder='" . $row['address'] . "'><br><br>");
+
+                echo ("<label for='postalCodeNew'>Postal Code</label><br>");
+                echo ("<input type='text' id='postalCodeNew' name='postalCodeNew' placeholder='" . $row['postalCode'] . "'><br><br>");
+
+                echo ("<input type='submit' value='Save edits'>");
+                echo ("</form><br><br>");
+                //close form
+        
+                if ($row['isAdmin'] === '1') {
+                    echo ("<a href='admin_control.php'>Admin Control Page</a><br>");
+                }
+
+                // log out button
+                echo ("<a href='logout.php'>Log Out</a>");
+            }
         } else {
+            echo "User not found";
+        }
+        ?>
 
-            if (
-                !empty($_POST['emailNew']) || !empty($_POST['phoneNumNew']) || !empty($_POST['addressNew'])
-                || !empty($_POST['postalCodeNew']) || !empty($_POST['passwordNew'])
-            ) {
+        <?php include "include/ad_long.php"; ?>
+        <h2>My Articles </h2>
 
-                // update info from form
-                if (!empty($_POST['emailNew'])) {
-                    $email = $_POST['emailNew'];
-                }
-                if (!empty($_POST['phoneNumNew'])) {
-                    $phoneNum = $_POST['phoneNumNew'];
-                }
-                if (!empty($_POST['addressNew'])) {
-                    $address = $_POST['addressNew'];
-                }
-                if (!empty($_POST['postalCodeNew'])) {
-                    $postalCode = $_POST['postalCodeNew'];
-                }
-                if (!empty($_POST['passwordNew'])) {
-                    $password = $_POST['passwordNew'];
-                }
+        <?php
+        if (!$isDisabled) {
+            echo "<a href='write_article.php'>[write new article]</a>";
+        }
 
-                try {
-                    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $server_username, $server_password);
-                    $sql2 = "UPDATE users SET email=?, phoneNum=?, address=?, postalCode=?, passwords=? WHERE username=?";
-                    $statement = $pdo->prepare($sql2);
-                    $statement->bindValue(1, $email);
-                    $statement->bindValue(2, $phoneNum);
-                    $statement->bindValue(3, $address);
-                    $statement->bindValue(4, $postalCode);
-                    $statement->bindValue(5, md5($password));
-                    $statement->bindValue(6, $session_username);
-                    $statement->execute();
-                    echo '<script>alert("Personal information updated successfully");</script>';
-                } catch (Exception $e) {
-                    echo '<script>alert("Error updating personal info");</script>';
-                }
+        $session_username = $_SESSION['username'];
+        $sql4 = "SELECT articleId, articleTitle FROM Articles WHERE username = '$session_username'";
+        $result4 = mysqli_query($conn, $sql4);
+
+        if (mysqli_num_rows($result4) > 0) {
+            while ($row = mysqli_fetch_assoc($result4)) {
+                echo ("<h3><a href='article.php?articleId=" . $row['articleId'] . "'>" . $row['articleTitle'] . "</a></h3>");
             }
+        } else {
+            echo "No articles published yet";
 
-            // Show updated info in placeholder
-            // Form to let users change info
-            $sql3 = "SELECT * FROM users WHERE username = '$session_username'";
-            $result3 = mysqli_query($conn, $sql3);
-            if (mysqli_num_rows($result3) > 0) {
-                while ($row = mysqli_fetch_assoc($result3)) {
-                    echo ("<h3>Username: " . $row['username'] . "</h3>");
-                    //username is unchangeable
-        
-                    echo ("<form action='profile.php?username=" . $_SESSION['username'] . "' method='post'>");
-                    //form open, submitted change will change the variables above
-        
-                    echo ("<label for='emailNew'>Email</label><br>");
-                    echo ("<input type='email' id='emailNew' name='emailNew' placeholder='" . $row['email'] . "'><br><br>");
-                    //current email in placeholder, email will update when typed into the input
-        
-                    echo ("<label for='passwordNew'>Password</label><br>");
-                    echo ("<input type='password' id='passwordNew' name='passwordNew' placeholder='●●●●●●●'><br><br>");
-
-                    echo ("<label for='phoneNumNew'>Phone number</label><br>");
-                    echo ("<input type='tel' id='phoneNumNew' name='phoneNumNew' placeholder='" . $row['phoneNum'] . "'><br><br>");
-
-                    echo ("<label for='addressNew'>Address</label><br>");
-                    echo ("<input type='text' id='addressNew' name='addressNew' placeholder='" . $row['address'] . "'><br><br>");
-
-                    echo ("<label for='postalCodeNew'>Postal Code</label><br>");
-                    echo ("<input type='text' id='postalCodeNew' name='postalCodeNew' placeholder='" . $row['postalCode'] . "'><br><br>");
-
-                    echo ("<input type='submit' value='Save edits'>");
-                    echo ("</form><br><br>");
-                    //close form
-        
-                    if ($row['isAdmin'] === '1') {
-                        echo ("<a href='admin_control.php'>Admin Control Page</a><br>");
-                    }
-
-                    // log out button
-                    echo ("<a href='logout.php'>Log Out</a>");
-                }
-            } else {
-                echo "User not found";
-            }
-            ?>
-
-            <?php include "include/ad_long.php"; ?>
-            <h2>My Articles <a href="write_article.php">[write new article]</a></h2>
-
-            <?php
-            $session_username = $_SESSION['username'];
-            $sql4 = "SELECT articleId, articleTitle FROM Articles WHERE username = '$session_username'";
-            $result4 = mysqli_query($conn, $sql4);
-
-            if (mysqli_num_rows($result4) > 0) {
-                while ($row = mysqli_fetch_assoc($result4)) {
-                    echo ("<h3><a href='article.php?articleId=" . $row['articleId'] . "'>" . $row['articleTitle'] . "</a></h3>");
-                }
-            } else {
-                echo "No articles published yet";
-            }
         }
 
         mysqli_close($conn);
