43950915796f1d7814749ee77ae1570279930462
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-04-06 06:45:38 +0900

    Update article.php

--- article.php
+++ article.php
@@ -102,7 +102,7 @@ $user = $_SESSION['username'];
                 $sql4 = "SELECT * FROM Comments WHERE articleId = '$articleId'";
                 $result4 = mysqli_query($conn, $sql4);
                 echo ("<br><h2>Comments</h2>");
-                echo("<div id = 'showcomments'>")
+                echo("<div id = 'showcomments'>");
                 //echo ("<h3><a href='write_comment.php?articleId=" . $articleId . "'>[Post new comment]</a></h3>");
                 if (mysqli_num_rows($result4) > 0) {
                     while ($row = mysqli_fetch_assoc($result4)) {
