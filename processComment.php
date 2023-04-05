f24e0097b21fe754a823e1f56ae7c639707260c0
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-04-06 06:55:27 +0900

    Update processComment.php

--- processComment.php
+++ processComment.php
@@ -65,6 +65,24 @@ try {
     $statement->execute();
 
     echo '<script>alert("Comment Posted");</script>';
+
+
+
+    $sql4 = "SELECT * FROM Comments WHERE articleId = '$articleId'";
+    $result4 = mysqli_query($conn, $sql4);
+    echo ("<br><h2>Comments</h2>");
+    echo("<div id = 'showcomments'>");
+    //echo ("<h3><a href='write_comment.php?articleId=" . $articleId . "'>[Post new comment]</a></h3>");
+    if (mysqli_num_rows($result4) > 0) {
+        while ($row = mysqli_fetch_assoc($result4)) {
+            echo ('<h3>' . $row["username"] . ' - ' . $row["commentBody"] . '</h3>');
+        }
+    } else {
+        echo "No comments yet";
+    }
+    echo("</div>");
+
+
 } catch (Exception $e) {
     echo '<script>alert("Error posting comment");</script>';
 }
