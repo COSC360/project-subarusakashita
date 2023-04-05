6bb40856c3bb271562eb3a5a4d621c4bcef1e07a
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-04-06 06:37:42 +0900

    Update article.php

--- article.php
+++ article.php
@@ -26,6 +26,25 @@ $user = $_SESSION['username'];
     <title>UniChannel | Article Page</title>
     <link rel="stylesheet" href="css/default.css">
     <link rel="stylesheet" href="css/article.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('#comment-form').submit(function(event) {
+            event.preventDefault();
+            $('#showcomment').load("processComment.php", {
+                articleId: "<?php echo htmlspecialchars($articleId, ENT_QUOTES) ?>",
+                messages: $('#comment').val()
+            })
+            .done(function(response) {
+                console.log(response);
+                // Handle the response from the server here
+            })
+            .fail(function(xhr, status, error) {
+                console.error(error);
+            });
+        });
+    });
+</script>
 </head>
 
 <body>
@@ -79,9 +98,11 @@ $user = $_SESSION['username'];
         
             if ($artDisabled !== '1') {
                 // Show Comments
+        
                 $sql4 = "SELECT * FROM Comments WHERE articleId = '$articleId'";
                 $result4 = mysqli_query($conn, $sql4);
                 echo ("<br><h2>Comments</h2>");
+                echo("<div id = 'showcomments'>")
                 //echo ("<h3><a href='write_comment.php?articleId=" . $articleId . "'>[Post new comment]</a></h3>");
                 if (mysqli_num_rows($result4) > 0) {
                     while ($row = mysqli_fetch_assoc($result4)) {
@@ -90,6 +111,7 @@ $user = $_SESSION['username'];
                 } else {
                     echo "No comments yet";
                 }
+                echo("</div>");
 
 
                 // Write Comment
@@ -104,7 +126,7 @@ $user = $_SESSION['username'];
                 if (isset($user) && $userDisabled !== '1') {
                     $articleId = $_GET['articleId'];
                     // if logged in
-                    echo '<form action ="processComment.php" method = "post">
+                    echo '<form id="comment-form" method = "post">
                       <input type="text" id="comment" name="comment" placeholder="Write comment here" required>
                       <input type = "hidden" name = "username" value = echo $user >
                       <input type = "hidden" name = "articleId" value = "' . $articleId . '" >
@@ -113,6 +135,7 @@ $user = $_SESSION['username'];
                       <input type="submit" value="Comment">
                       </form>';
                 }
+
             }
             // Related articles
             $sql6 = "SELECT * FROM Articles WHERE categoryId = '$categoryId' ORDER BY commentNum LIMIT 3";
