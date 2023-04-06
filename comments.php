0bd7dc8dfce6f752a2e7c206b4fb14bd16cb7904
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-04-06 09:30:00 +0900

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,10 +1,20 @@
 <?php
+$servername = "cosc360.ok.ubc.ca";
+$server_username = "83395822";
+$server_password = "83395822";
+$dbname = "db_83395822";
+
+$conn = mysqli_connect($servername, $server_username, $server_password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 if (isset($_GET["articleId"])) {
     $articleId = $_GET["articleId"];
     // rest of the code
     $sql4 = "SELECT * FROM Comments WHERE articleId = '$articleId'";
     $result4 = mysqli_query($conn, $sql4);
-    echo ("<br><h2>Comments</h2>");
+    //echo ("<br><h2>Comments</h2>");
     echo("<div id = 'showcomments'>");
     //echo ("<h3><a href='write_comment.php?articleId=" . $articleId . "'>[Post new comment]</a></h3>");
     if (mysqli_num_rows($result4) > 0) {
