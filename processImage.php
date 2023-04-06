3ecf8246c80f319afb70fb05fb7980a13fdd8874
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-06 15:04:01 -0700

    Update processImage.php

--- processImage.php
+++ processImage.php
@@ -21,7 +21,7 @@ $username = $_SESSION['username'];
 
 $file = basename($_FILES['userImage']['name']);
 $imageFileType = strtolower(pathinfo($file, PATHINFO_EXTENSION));
-echo '<script>alert(' . $username . ');</script>';
+echo '<script>alert("$username");</script>';
 
 if (
     getimagesize($_FILES['userImage']['tmp_name']) !== false &&
@@ -50,27 +50,21 @@ if (
     } else {
         // user does not have image yet
 
-        // if (move_uploaded_file($_FILES['userImage']['tmp_name'], $file)) {
-        //     echo '<script>alert("Upload successful");</script>';
-        // } else {
-        //     echo '<script>alert("Error during upload");</script>';
-        // }
-
         $imagedata = file_get_contents($_FILES['userImage']['tmp_name']);
-        $sql3 = "INSERT INTO Images (username, fileType, file) VALUES('$username', '$imageFileType', null)";
-        $conn->query($sql3);
-        // $stmt = mysqli_stmt_init($conn);
-        // mysqli_stmt_prepare($stmt, $sql3);
-        // $null = null;
-        // mysqli_stmt_bind_param(
-        //     $stmt,
-        //     "isb",
-        //     $username,
-        //     $imageFileType,
-        //     $null
-        // );
-        // mysqli_stmt_send_long_data($stmt, 2, $imagedata);
-        // $result = mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+        $sql3 = "INSERT INTO Images (username, fileType, file) VALUES(?,?,?)";
+
+        $stmt = mysqli_stmt_init($conn);
+        mysqli_stmt_prepare($stmt, $sql3);
+        $null = null;
+        mysqli_stmt_bind_param(
+            $stmt,
+            "isb",
+            $username,
+            $imageFileType,
+            $null
+        );
+        mysqli_stmt_send_long_data($stmt, 2, $imagedata);
+        $result = mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
 
     }
     echo '<script>alert("Image uploaded 👍");</script>';
