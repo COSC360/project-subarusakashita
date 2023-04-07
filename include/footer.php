6e8e3f445f797a85799bcc85a8430e8992ed19d3
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-04-08 07:52:35 +0900

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <?php
 echo ("<footer>
         <p>Footer Copyright info and etc</p>
-        <a href = 'contact_form.php'>Contact Form </a>
+        <p><a href = 'contact_form.php'>Contact Form </a></p>
     </footer>");
 ?>
