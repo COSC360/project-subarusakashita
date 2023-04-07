b8c4e483efd401c67924de5987da69c0e9c6abc7
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-04-08 06:03:50 +0900

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,26 +1,30 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-  </head>
-  <body>
-    <?php
-      mb_internal_encoding("UTF-8");
-      $to = $_POST['to'];
-      $title = $_POST['title'];
-      $content = $_POST['content'];
-      if (!empty($_POST["name"])) {
-        $name = strip_tags($_POST["name"]);
-        $email = strip_tags($_POST["email"]);
-        $content = $content."\nFrom Name: ".$name."\nEmail: ".$email;
-    }
-     
-      if(mb_send_mail($to, $title, $content)){
-        echo "Mail successfully sent";
-        header ("Location: main.php");
-      } else {
-        echo "Sending failed";
-      };
-    ?>
-  </body>
-</html>
+
+<?php
+  session_start();
+  mb_internal_encoding("UTF-8");
+  $to = $_POST['to'];
+  $title = $_POST['title'];
+  $content = "Content: ".$_POST['content'];
+  if (!empty($_POST["name"])) {
+    $name = strip_tags($_POST["name"]);
+    $email = strip_tags($_POST["email"]);
+    $content = $content."\nFrom Name: ".$name."\nEmail: ".$email;
+}
+else{
+  $username = $_SESSION["username"];
+  $sql = "SELECT email FROM users WHERE username = '$username' ";
+  $result = mysqli_query($conn, $sql);
+  if ($result) {
+      $row = mysqli_fetch_assoc($result);
+      $email = $row['email'];
+      $content = $content."\nFrom Name: ".$username."\nEmail: ".$email;
+  }
+}
+  
+  if(mb_send_mail($to, $title, $content)){
+    echo "Mail successfully sent";
+    header ("Location: main.php");
+  } else {
+    echo "Sending failed";
+  };
+?>
