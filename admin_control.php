3625ebeea66b54249f6c9fc90d930f1246247b74
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-07 12:13:33 -0700

    Update admin_control.php

--- admin_control.php
+++ admin_control.php
@@ -137,6 +137,7 @@ if (mysqli_num_rows($result) > 0) {
                     mysqli_stmt_bind_result($stmt, $type, $image);
                     mysqli_stmt_fetch($stmt);
                     mysqli_stmt_close($stmt);
+                    echo $stmt . $image. $type . "<br>";
                     if ($image !== null) {
                         echo '<td><img id="profile" src="data:image/' . $type . ';base64,' . base64_encode($image) . '"></td>';
                     } else {
