0dc4cd36826fff3f6ed214b40837a0fa55a35145
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2023-04-08 07:54:18 +0900

    Update main.php

--- main.php
+++ main.php
@@ -99,6 +99,7 @@ if (!$conn) {
     <!-- <?php include "include/footer.php" ?> -->
     <footer>
         <p>Footer Copyright info and etc</p>
+        <p><a href = 'contact_form.php'>Contact Form </a></p>
     </footer>
 </body>
 
