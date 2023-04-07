d99873fa27074848efa283a4f9a46df4eade5a2d
karenzhg <97936387+karenzhg@users.noreply.github.com>
2023-04-07 12:07:57 -0700

    Update admin_control.php

--- admin_control.php
+++ admin_control.php
@@ -140,7 +140,7 @@ if (mysqli_num_rows($result) > 0) {
                     if ($image !== null) {
                         echo '<td><img id="profile" src="data:image/' . $type . ';base64,' . base64_encode($image) . '"></td>';
                     }
-                    
+
                     echo ("
                     <td>" . $row['username'] . "</td>
                     <td>" . $row['email'] . "</td>
@@ -160,12 +160,14 @@ if (mysqli_num_rows($result) > 0) {
                     $stmt = mysqli_stmt_init($conn);
                     mysqli_stmt_prepare($stmt, $sql6);
                     mysqli_stmt_bind_param($stmt, "s", $row['username']);
-                    $result2 = mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+                    $result6 = mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
                     mysqli_stmt_bind_result($stmt, $type, $image);
                     mysqli_stmt_fetch($stmt);
                     mysqli_stmt_close($stmt);
-                    echo '<td><img id="profile" src="data:image/' . $type . ';base64,' . base64_encode($image) . '"></td>';
-
+                    if ($image !== null) {
+                        echo '<td><img id="profile" src="data:image/' . $type . ';base64,' . base64_encode($image) . '"></td>';
+                    }
+                    
                     echo ("
                     <td>" . $row['username'] . "</td>
                     <td>" . $row['email'] . "</td>
